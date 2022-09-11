1c54555dd96c01f5983798148ec03625458cdc18
Artem Khromov <artem.a.khromov@gmail.com>
2022-09-11 22:11:52 +0300

    Added 3rd Harry potter book into sample data

--- sample-data-preparation/prepare.py
+++ sample-data-preparation/prepare.py
@@ -50,7 +50,6 @@ def Harry():
     os.remove("harry.txt")    
     os.remove("harry-2.txt")
     os.rename("harry-3.txt", "data/harry-3.txt")
-
     
 def Harry2():
     fin = open("2harry.txt", 'r')
@@ -77,8 +76,34 @@ def Harry2():
     os.remove("2harry-2.txt")
     os.rename("2harry-3.txt", "data/2harry-3.txt")
 
+def Harry3():
+    fin = open("3harry.txt", 'r')
+    content = fin.readlines()
+    fin.close()
+    content = content[12:]
+    fout = open("3harry-2.txt", 'w')
+    for line in content:
+        if "J.K. Rowling" in line:
+            continue
+        if line == '\n':
+            continue
+        print(line, file=fout)
+    fout.close()
+    fin = open("3harry-2.txt", 'r')
+    content = fin.read()
+    fin.close()
+    fout = open("3harry-3.txt", 'w')
+    content = content.replace('\n', ' ').replace(',', '').replace("\"", "").replace(":", '').replace(";", '').replace('(', ' ').replace(')', ' ').replace('!', '.').replace('?', '.').replace("'", '').replace('.', ' ').replace('-', ' ').replace('  ', ' ').replace('’', '').replace("—", "").replace("”", "").replace("“", "").lower()
+    content = content.replace("  ", " ").replace("  ", " ").replace("  ", " ").replace("  ", " ").replace("  ", " ").replace("  ", " ").replace('\\', '')
+    print(content, file=fout)
+    fout.close()
+    os.remove("3harry.txt")    
+    os.remove("3harry-2.txt")
+    os.rename("3harry-3.txt", "data/3harry-3.txt")
+
 
 if __name__ == "__main__":
     Shakespeare()
     Harry()
     Harry2()
+    Harry3()
